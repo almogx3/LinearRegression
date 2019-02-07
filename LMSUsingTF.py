3515e9a0c04d65469f9cd91a861c588b7dc1d033
Almog <almogx3@gmail.com>
2019-02-07 23:06:30 +0200

    bugfix and th change

--- LMSUsingTF.py
+++ LMSUsingTF.py
@@ -5,8 +5,8 @@ from progressbar import *
 from LMSFunctions import LMS_function_class
 
 # Parameters
-learning_rate = 0.01
-num_steps = 500
+learning_rate = 0.1
+num_steps = 1000
 batch_size = 20
 display_step = 50
 
@@ -23,20 +23,23 @@ noise_amp = 1
 # Network parameters
 n_hidden_1 = 3
 n_output_classes = 1
+n_input = d
 
-R = LMS_function_class.create_cov_matrix(d, k)
-train_x, train_y = LMS_function_class.create_random_vectors(n, R, np.zeros((d,)))
+LMS = LMS_function_class()
+R = LMS.create_cov_matrix(d, k)
+train_x, train_y = LMS.create_random_vectors(n, R, np.zeros((d,)))
+train_x = train_x[1:, :].T
 train_y = train_y + noise_amp * np.random.rand(train_y.shape[0], )
 
 # tf Graph Input
-X = tf.placeholder("float")
-Y = tf.placeholder("float")
+X = tf.placeholder("float64", shape=(n, d))
+Y = tf.placeholder("float64", shape=(n, ))
 
 # set weights and bias
-W = tf.Variable(np.random.randn(d), name="weight")
-b = tf.Variable(np.random.randn(), name="bias")
+W = tf.Variable(np.random.randn(d, 1), name="weight", )
+b = tf.Variable(np.random.randn(1, 1), name="bias")
 
-pred = tf.add(tf.multiply(X, W), b)
+pred = tf.add(tf.linalg.matmul(X, W), b)
 
 # MSE
 cost = tf.reduce_sum(tf.pow(pred - Y, 2)) / n
@@ -59,19 +62,20 @@ with tf.Session() as sess:
             c = sess.run(cost, feed_dict={X: train_x, Y: train_y})
             print("step", '%04d' % step, "cost=", "{:.9f}".format(c), "W=", sess.run(W), "b", sess.run(b))
 
-print("optimization Finished")
-training_cost = sess.run(cost, feed_dict={X: train_x, Y: train_y})
-print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+    print("optimization Finished")
+    training_cost = sess.run(cost, feed_dict={X: train_x, Y: train_y})
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
 
-# Testing example, as requested (Issue #2)
-test_X, test_Y = LMS_function_class.create_random_vectors(n, R, np.zeros((d,)))
-test_Y = test_Y + noise_amp * np.random.rand(test_Y.shape[0], )
-print("Testing... (Mean square loss Comparison)")
-testing_cost = sess.run(
-    tf.reduce_sum(tf.pow(pred - Y, 2)) / (n),
-    feed_dict={X: test_X, Y: test_Y})  # same function as cost above
-print("Testing cost=", testing_cost)
-print("Absolute mean square loss difference:", abs(
-    training_cost - testing_cost))
+    # Testing example, as requested (Issue #2)
+    test_x, test_y = LMS.create_random_vectors(n, R, np.zeros((d,)))
+    test_x = test_x[1:, :].T
+    test_y = test_y + noise_amp * np.random.rand(test_y.shape[0], )
+    print("Testing... (Mean square loss Comparison)")
+    testing_cost = sess.run(
+        tf.reduce_mean(tf.pow(pred - Y, 2)),
+        feed_dict={X: test_x, Y: test_y})  # same function as cost above
+    print("Testing cost=", testing_cost)
+    print("Absolute mean square loss difference:", abs(
+        training_cost - testing_cost))
 
 print("done")
